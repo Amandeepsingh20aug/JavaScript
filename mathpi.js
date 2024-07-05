12904b6b1c76a56501eefc2235456047f71675d4
amandeepsingh1929 <as0632568@gmail.com>
2024-07-06 01:32:03 +0530

    Learn advance objects .

--- mathpi.js
+++ mathpi.js
@@ -1,3 +1,28 @@
-const discreptor = Object.getOwnPropertyDescriptor(Math,"PI")
+// const discreptor = Object.getOwnPropertyDescriptor(Math,"PI")
 
+// console.log(discreptor);
+
+const user = {
+  userName : "Aman",
+  isAvailable : true,
+  age : 23,
+
+  orderChai : function() {
+    console.log("chai nhi bni");
+  }
+}
+
+
+Object.defineProperty(user,'userName' , {
+  writable: false,
+  enumerable : false
+})
+
+const discreptor= Object.getOwnPropertyDescriptor(user,'userName')
 console.log(discreptor);
+
+for (let [key,value] of Object.entries(user)) {
+   if(typeof value !== "function"){
+      console.log(`${key} : ${value}`);
+   }
+}
